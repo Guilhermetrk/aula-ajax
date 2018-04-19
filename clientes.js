bcc7af9d534a86fee5b07e89c5516cbf66b1cb12
Guilherme <guizintreuko@hotmail.com>
2018-04-18 21:45:47 -0300

    add datepicker

--- clientes.js
+++ clientes.js
@@ -24,8 +24,58 @@ $(function() {
                         +'</tr>';
 
                  $("tbody").append(tr);
-            });
-        });
+            }); // fin do forech
+
+        });  // fim do getJSON
    // });
 
+   $("fieldset button").click(function(){
+
+        var nome = $("input[name=nome]").val();
+        var email = $("input[name=email]").val();
+        var cep = $("input[name=cep]").val();
+        var dataNasc = $("input[name=data-nasc]").val();
+
+        var tr = '<tr>'
+                +'<td>'+ nome +'</td>'
+                +'<td>'+ email +'</td>'
+                +'<td>'+ cep +'</td>'
+                +'<td>'+ dataNasc +'</td>'
+                +'</tr>';
+
+        var tr2 = $(tr).hide().fadeIn(2000);
+
+         $("tbody").append(tr2);
+
+   }); // fim do click
+
+   $("input[name=cep]").keydown(function(ev){
+        console.log(ev);
+
+        if ((ev.keyCode >= 48 && ev.keyCode <= 57) || ev.keyCode == 8 )
+        {
+            return true;
+        } else {
+            return false;
+        }
+   }); //fim keydown
+
+   $("input[name=data-nasc]").datepicker({
+       dateFormat: "dd/mm/yy"
+   });
+   
+   $("input[name=email]").blur(function(){
+
+        var valor = $(this).val();
+
+        var regex = /^[a-zA-z0-9\.\-_]*@[a-z0-9]*\.[a-z\.]*$/gm;
+
+        if(regex.test(valor) == false)
+        {
+            alert("Email inválido");
+        }
+
+   }); // fim blur
+
 });
+
